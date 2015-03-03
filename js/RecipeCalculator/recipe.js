8dbdfc9993a7accea3567c74587c69702d88b62c
nlaert <nlaert@gmail.com>
2015-03-03 17:07:33 +0000

    added Recipe Calculator

---
+++ js/RecipeCalculator/recipe.js
@@ -0,0 +1,83 @@
+function Ingredient (name, amount)
+{
+	this.name = name;
+	this.amount = amount;
+};
+
+Ingredient.prototype.toString = function() {
+	if(this.hasAmount())
+		return this.amount + ' ' + this.name;
+	return this.name; 
+};
+
+Ingredient.prototype.hasAmount = function() {
+	return this.amount > 0;
+};
+
+var ingredients = [];
+/************ HTML ids to be used ********************/
+var recipeInput = 'originalRecipe';
+var originalPortionsInput = 'originalPortions';
+var newPortionsInput = 'newPortions';
+var resultDiv = 'resultDiv';
+var resultUl = 'result';
+
+function execute()
+{
+	readRecipe();
+	var originalPortions = document.getElementById(originalPortionsInput).value;
+	var newPortions = document.getElementById(newPortionsInput).value;
+	calculateByPortions(originalPortions, newPortions);
+	printRecipe();
+}
+
+function readRecipe()
+{
+	var text = document.getElementById(recipeInput).value;
+	var lines = text.split(/\n/);
+	console.log(lines);
+	lines.forEach(function(line)
+	{
+		var ing = newIngredient(line);
+		ingredients.push(ing);
+	});
+
+}
+
+function newIngredient(line)
+{
+	line = line.trim();
+	var firstChar = line.charAt(0);
+	if ((firstChar != ',' && firstChar!='.') && (firstChar < '0' || firstChar > '9'))
+		return new Ingredient(line);
+	var space = line.indexOf(' ');
+	if (space < 0)
+		throw 'Invalid Format';
+	var amount = parseInt(line.substring(0,space));
+	return new Ingredient(line.substring(space + 1), amount);
+}
+
+function calculateByPortions(originalPortions, newPortions)
+{
+	var percent = newPortions / originalPortions;
+	ingredients.forEach(function(ingredient)
+		{
+			calculate(ingredient, percent);
+		});
+}
+
+function calculate(ingredient, percent)
+{
+	if (ingredient.hasAmount())
+		ingredient.amount = ingredient.amount * percent;
+}
+
+function printRecipe()
+{
+	var newRecipe = '';
+	ingredients.forEach(function (elem) {
+		 newRecipe = newRecipe + '<li class="list-group-item">' + elem.toString() + '</li>';
+	});
+	document.getElementById(resultDiv).className = 'row';
+	document.getElementById(resultUl).innerHTML = newRecipe;
+}
